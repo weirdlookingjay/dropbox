9c1af29b6e475098e4ea77823dc6047c461929f2
Joseph Clark <weirdlookingjay@live.com>
2023-12-01 10:12:27 -0500

    Updated Dashboard Page with uploaded content

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,11 +1,36 @@
 import Dropzone from "@/components/Dropzone";
+import { db } from "@/firebase";
 import { auth } from "@clerk/nextjs";
+import { collection, getDocs } from "firebase/firestore";
+import { FileType } from "@/typings";
+import TableWrapper from "@/components/table/TableWrapper";
 
-function Dashboard() {
+async function Dashboard() {
   const { userId } = auth();
+
+  const docResults = await getDocs(collection(db, "users", userId!, "files"));
+  const skeletonFiles: FileType[] = docResults.docs.map((doc) => ({
+    id: doc.id,
+    filename: doc.data().filename || doc.id,
+    timestamp: new Date(doc.data().timestamp?.seconds * 1000) || undefined,
+    fullName: doc.data().fullName,
+    downloadURL: doc.data().downloadURL,
+    type: doc.data().type,
+    size: doc.data().size,
+  }));
+
+  // console.log(skeletonFiles);
+
   return (
-    <div>
+    <div className="border-t">
       <Dropzone />
+
+      <section className="container space-y-5">
+        <h2 className="font-bold">All Files</h2>
+        <div>
+          <TableWrapper skeletonFiles={skeletonFiles} />
+        </div>
+      </section>
     </div>
   );
 }
